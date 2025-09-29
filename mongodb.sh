8129339c74a3e38633fbafb1ba0978ed64f31ca2
viho-kernel <vihari.reddy1802@gmail.com>
2025-09-29 12:21:47 +0530

    Initial commit

---
+++ mongodb.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started execution at $TIMESTAMP" &>>$LOGFILE
+VALIDATE() {
+    if [ $1 -ne 0]
+    then
+        echo -e "$R Error:: $2 is failed.$N"
+    else
+        echo -e "$G Success:: $2 is successful.$N"
+    fi
+}
+
+if [ $ID -ne 0 ] 
+then    
+  echo -e "$R Error:: This script must be run as root or with sudo.$N"
+  exit 1
+else
+    echo -e "$G You are running this script as root.$N"
+
+fi
