d83c5ad7317b4e98fd9b98b1519b0ecf68279797
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-02 23:00:25 +0530

    initial commit

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "rabbitmq" "mysql" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-    if [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
