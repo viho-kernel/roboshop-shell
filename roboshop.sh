8a816099bcb6d49767419ccd4ea80cd3f49ce34e
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-02 22:55:30 +0530

    initial commit

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-00efbfec6ad5ec04a
+INSTANCES=("mongodb" "redis" "rabbitmq" "mysql" "catalogue" "user" "cart" "shipping" "payment" "frontend" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb"] || [ $i == "mysql"] || [ $i == "shipping"]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID
+
+done
