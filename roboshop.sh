4e0e7063bca7b2a6727dde3933a8d8a3652417d9
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-02 23:11:10 +0530

    initial commit

--- roboshop.sh
+++ roboshop.sh
@@ -12,6 +12,6 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=stack,Value=$i}]'
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=stack,Value=$i}]"
 
 done
