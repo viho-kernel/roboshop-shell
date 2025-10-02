7820ccc7b2a662dc615194e07b14e42cd89b6d36
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-03 00:05:03 +0530

    initial commit

--- roboshop.sh
+++ roboshop.sh
@@ -8,9 +8,9 @@ do
     echo "instance is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
-        INSTANCE_TYPE="t3.small"
+        INSTANCE_TYPE="t3.medium"
     else
-        INSTANCE_TYPE="t2.micro"
+        INSTANCE_TYPE="t3.micro"
     fi
     IP_address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance, Tags=[{Key=stack,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "IP address of $i is: $IP_address"
