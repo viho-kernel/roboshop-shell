9531393c18523e659c828bf20ff81faa6943c73e
viho-kernel <vihari.reddy1802@gmail.com>
2025-10-03 11:16:27 +0530

    Latest commit

--- ec2.sh
+++ ec2.sh
@@ -13,8 +13,8 @@ do
     else
         INSTANCE_TYPE="t3.micro"
     fi
-    
-    IP_address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PublicIpAddress' --output text)
+
+    IP_address=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "Ip address of $i: $IP_address"
 
